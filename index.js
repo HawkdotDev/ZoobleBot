2d02f7b8b5fb998262a5a5f35c950d51e6530f0e
Dwaipayan Dutta <dwaipayandutta.official@gmail.com>
2024-03-14 00:13:44 +0530

    removed toolbar

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var bookmarkToolbar = document.getElementById("PersonalToolbar") || document.getElementById("bookmark-bar");
+    if (bookmarkToolbar) {
+        bookmarkToolbar.style.display = "none";
+    }
+}
